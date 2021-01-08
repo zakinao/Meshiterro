5b4fdb132b454c41e53e032e756cdd67fa2eb214
zakinao <zakinaororo@gmail.com>
2021-01-08 10:26:45 +0000

    create post

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '820103659bb873858d5542ea583d09ba1b0127537c137f3b91f4da9a32d9c5680ebc86a4107f6d6406f59cc2405d39e8ed73b708644897c4f7d7f4c3a79318f4'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+end
