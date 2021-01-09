91df09aa6ea89183b9ce6ddd40fce7cc63ebf2d7
zakinao <zakinaororo@gmail.com>
2021-01-09 07:04:41 +0000

    add mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   root 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
